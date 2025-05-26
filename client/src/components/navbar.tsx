613df5896fc48fd8e7d61e415d22a73e41394c4f
CicadaMusic <team@cicadamusic.net>
2025-05-26 11:01:13 +0300

    fixed cursor-pointer

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -9,28 +9,25 @@ export default function NavBar() {
       <div className="max-w-8xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-10">
           <div className="flex items-center">
-            <Link href="/">
-              <div className="flex-shrink-0 py-2 flex gap-2 md:gap-3">
-                <Image
-                  className="h-6 w-auto"
-                  src="/house2.png"
-                  alt="Confy Logo"
-                  height={25}
-                  width={25}
-                />
-                <h2>
-                  Census Surveyor<span className="hidden sm:inline"> - Houshold Statistics</span>
-                </h2>
-              </div>
-            </Link>
+            <div className="flex-shrink-0 py-2 flex gap-2 md:gap-3">
+              <Image
+                className="h-6 w-auto"
+                src="/house2.png"
+                alt="Confy Logo"
+                height={25}
+                width={25}
+              />
+              <h2>
+                Census Surveyor<span className="hidden sm:inline"> - Houshold Statistics</span>
+              </h2>
+            </div>
           </div>
           <div
             className="flex items-center justify-center gap-3 flex-shrink-0"
             id="clerk-user-container"
           >
-            <span className="cursor-pointer flex items-center">
-            </span>
-            <span className="cursor-pointer mx-1">
+            <span className="flex items-center"></span>
+            <span className="mx-1">
               <ModeToggle />
             </span>
           </div>
